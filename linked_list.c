2775fc372f423bac8ebad9748b09edfa399d5b91
piptarou <piptarou@gmail.com>
2025-03-12 22:38:45 -0300

    renamed header file to match reqs, added structs and 1st function

--- linked_list.c
+++ linked_list.c
@@ -1,1 +1,24 @@
-// Linked_list.c
+#include "singly_linked_list.h"
+#include <stdlib.h>
+
+typedef struct Node {
+    void* data;
+    struct Node * next;
+} Node;
+
+struct LinkedList {
+    size_t size;
+    struct Node * head;
+    struct Node * tail;
+};
+
+LinkedList* list_create(void) {
+    LinkedList* output = malloc(sizeof(LinkedList));
+    if (output == NULL) {
+        return NULL;
+    }
+    output->size = 0;
+    output->head = NULL;
+    output->tail = NULL;
+    return output;
+}
