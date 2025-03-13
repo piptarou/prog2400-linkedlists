57e9afe0f5fe6a778dc7fdd39ac4095c06c464fe
piptarou <piptarou@gmail.com>
2025-03-13 03:29:59 -0300

    list_create and size_t in main.c

--- main.c
+++ main.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "linked_list.h"
+#include "singly_linked_list.h"
 
+int run_test_cases() {
+
+    LinkedList * myLinkedList = list_create();
+
+    size_t actualSize = list_size(myLinkedList);
+    if (actualSize != 0) {
+        printf("FAILED test case due to size failure on empty list\n");
+        return 1;
+    }
+}
+
+int main() {
+    printf("welcome to the linked list test program!\n");
+    int result = run_test_cases();
+    if (result == 0){
+        printf("Great success!\n");
+    } else {
+        printf("FAILED!!");
+    }
+    return 0;
+}
